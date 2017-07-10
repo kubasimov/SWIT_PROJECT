956b73d73d8a48283802b61d2818882d79c513c9
KubAsimov <kubasimov@gmail.com>
2017-07-10 11:59:09 +0200

    wczytywanie pliku słownika .dsl i zapis do modelu - cz.1

---
+++ Console/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Console
+{
+    public class Tests
+    {
+        [Fact]
+       public void ReadSubString()
+        {
+            var text = "\tKaczka";
+            var sub = text.Substring(0,1);
+
+            Assert.Equal("\t",sub);
+        }
+    }
+}
